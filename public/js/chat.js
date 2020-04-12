0ce8c64af969035a3651d5e04bf8e9098a2f68b2
Lucien George <luciengeorge95@gmail.com>
2020-04-12 12:23:03 +0100

    rendering messages on the page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,38 @@
 const socket = io(); // available because it is loaded after the socket.io script in index.html
 
+const $form = document.getElementById('message-form');
+const $messageInput = document.getElementById('message');
+const $formSubmit = document.getElementById('submit-cta');
+const $locationCta = document.getElementById('send-location');
+const $messages = document.getElementById('messages');
+
+// templates
+const messageTemplate = document.getElementById('message-template').innerHTML;
+const locationTemplate = document.getElementById('location-template').innerHTML;
+
 socket.on('message', (message) => {
-  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message
+  });
+  $messages.insertAdjacentHTML('beforeend', html);
 });
 
-const form = document.getElementById('message-form');
+socket.on('locationMessage', (location) => {
+  const html = Mustache.render(locationTemplate, {
+    location
+  });
+  $messages.insertAdjacentHTML('beforeend', html);
+});
 
-form.addEventListener('submit', (event) => {
+$form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const messageInput = event.target.elements.message
-  socket.emit('sendMessage', messageInput.value, (error) => {
+  $formSubmit.setAttribute('disabled', 'disabled');
+
+  socket.emit('sendMessage', $messageInput.value, (error) => {
+    $formSubmit.removeAttribute('disabled');
+    $messageInput.value = '';
+    $messageInput.focus();
+
     if (error) {
       return console.log(error)
     }
@@ -17,16 +40,16 @@ form.addEventListener('submit', (event) => {
   });
 });
 
-const locationCta = document.getElementById('send-location');
-
-locationCta.addEventListener('click', () => {
+$locationCta.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('geolocation is not supported by your browser')
   }
+  $locationCta.setAttribute('disabled', 'disabled');
 
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit('sendLocation', { latitude: position.coords.latitude, longitude: position.coords.longitude }, () => {
       console.log('location has been shared');
+      $locationCta.removeAttribute('disabled');
     });
   });
 });
